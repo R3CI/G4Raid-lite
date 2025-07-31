5b6e8e54814f105e68cebee7061729e2c3731163
r3ci <139361332+R3CI@users.noreply.github.com>
2025-07-31 10:58:01 +0200

    update run to py

---
+++ RUN.py
@@ -0,0 +1,18 @@
+import os
+import webbrowser
+import subprocess
+import sys
+
+os.system('cls')
+
+print('[*] Discord Support: https://discord.gg/spamming')
+print('[*] Github Repo: https://github.com/r3ci/g4spam')
+print()
+
+webbrowser.open('https://discord.gg/spamming')
+webbrowser.open('https://t.me/g4tool')
+webbrowser.open('https://g4tools.top')
+
+subprocess.run([sys.executable, 'main.py'])
+
+input('Press Enter to continue...')
