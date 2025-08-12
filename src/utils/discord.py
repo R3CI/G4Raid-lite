8be60806ab283743697c1841f59b627ae25fd867
R3CI <extiverooo@proton.me>
2025-08-12 15:13:44 +0200

    watermarks

--- src/utils/discord.py
+++ src/utils/discord.py
@@ -1,3 +1,14 @@
+# This code is the property of R3CI.
+# Unauthorized copying, distribution, or use is prohibited.
+# Licensed under the GNU General Public License v3.0 (GPL-3.0).
+# For more details, visit https://github.com/R3CI/G4Spam
+
+# This code is not the best as i honestly dont care much about it its made to work well and i do not need it to be good code overall as i dont update this often
+# Only the paid version will get updates often this is a side thing nothing crazy
+# Remember this is literary the only up to date FREE tool out on github all the other ones are old or skids from 2023
+# If you wana get more features with the cost of flgging ur stuff do but you will make ur tokens flagged
+
+
 from src import *
 
 class discord:
