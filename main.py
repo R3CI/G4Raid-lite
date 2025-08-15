5d23df93cac520f884af4612db94bfb096a108fd
r3ci <139361332+R3CI@users.noreply.github.com>
2025-08-15 12:53:58 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
-# This code is the property of R3CI.
-# Unauthorized copying, distribution, or use is prohibited.
-# Licensed under the GNU General Public License v3.0 (GPL-3.0).
-# For more details, visit https://github.com/R3CI/G4Spam
-
-# This code is not the best as i honestly dont care much about it its made to work well and i do not need it to be good code overall as i dont update this often
-# Only the paid version will get updates often this is a side thing nothing crazy
-# Remember this is literary the only up to date FREE tool out on github all the other ones are old or skids from 2023
-# If you wana get more features with the cost of flgging ur stuff do but you will make ur tokens flagged
-
 import os
 import webbrowser
 import time
+import ctypes
+
 os.system('cls')
 
-print('The free version has been discontinued, if you want to use g4spam buy the paid version for only 10 USD here https://g4tools.top')
+hwnd = ctypes.windll.kernel32.GetConsoleWindow()
+ctypes.windll.user32.SetWindowPos(hwnd, -1, 0, 0, 0, 0, 0x0001 | 0x0002)
+
+print('The free version has been discontinued')
+print('Buy the FULL premium version for only 10 USD on https://g4tools.top')
 time.sleep(2.5)
 webbrowser.open('https://g4tools.top')
 webbrowser.open('https://discord.gg/spamming')
